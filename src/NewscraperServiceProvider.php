4aa166433532b7d4a8f827672b15b71aad9c2556
unknown <a.partadzhiev@gmail.com>
2020-04-11 16:52:44 +0300

    Initial Service Provider

---
+++ src/NewscraperServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Nxvhm\Newscraper;
+
+use Illuminate\Support\ServiceProvider;
+
+class NewscraperServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
